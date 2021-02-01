ec810e313b0cb0bfb60a2fa18efc361b86846139
Radek Molenda <radek.molenda@gmail.com>
2021-02-02 00:22:14 +0100

    chore: better header

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,40 @@
 import React from "react";
 import { Link } from "gatsby";
+import Logo from "../images/logo-w150.jpeg";
+
 import styles from "./navigation.module.css";
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
-  </nav>
+  <>
+    <div className={styles.navigation}>
+      <div className={styles.navigationLogo}>
+        <Link to="/">
+          <img
+            alt="Biblioteka Żukowo"
+            src={Logo}
+            className={styles.navigationLogoImg}
+          />
+        </Link>
+      </div>
+      <nav role="navigation" className={styles.navigationLinks}>
+        <div className={styles.navigation}>
+          <Link to="/" className={styles.navigationItem}>
+            Home
+          </Link>
+          <Link to="/projekty/" className={styles.navigationItem}>
+            Projekty
+          </Link>
+          <Link to="/aktualnosci/" className={styles.navigationItem}>
+            Aktualności
+          </Link>
+          <Link to="/o-nas/" className={styles.navigationItem}>
+            O nas
+          </Link>
+          <Link to="/kontakt/" className={styles.navigationItem}>
+            Kontakt
+          </Link>
+        </div>
+      </nav>
+    </div>
+  </>
 );
