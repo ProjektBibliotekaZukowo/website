99bec1370b448d8b847bd29e1cb9c8f159627b97
Radek Molenda <radek.molenda@gmail.com>
2021-02-02 22:54:03 +0100

    Fix build

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,10 @@ import get from "lodash/get";
 import { Helmet } from "react-helmet";
 import Hero from "../components/hero";
 import Layout from "../components/layout";
-import ArticlePreview from "../components/article-preview";
 
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, "props.data.site.siteMetadata.title");
-    const posts = get(this, "props.data.allContentfulBlogPost.edges");
     const [libraryImage] = get(this, "props.data.libraryImage.nodes");
     const [logoImage] = get(this, "props.data.logoImage.nodes");
 
@@ -18,18 +16,6 @@ class RootIndex extends React.Component {
         <div style={{ background: "#fff" }}>
           <Helmet title={siteTitle} />
           <Hero data={libraryImage} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
         </div>
       </Layout>
     );
@@ -64,49 +50,6 @@ export const pageQuery = graphql`
           background: "rgb:8CA5C4"
         ) {
           ...GatsbyContentfulFluid_tracedSVG
-        }
-      }
-    }
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      edges {
-        node {
-          title
-          slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-          description {
-            childMarkdownRemark {
-              html
-            }
-          }
-        }
-      }
-    }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
-      edges {
-        node {
-          name
-          shortBio {
-            shortBio
-          }
-          title
-          heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
         }
       }
     }
