bd95a0829257ab4002072597c301046cbb3b311a
Radek Molenda <radek.molenda@gmail.com>
2022-02-06 18:13:03 +0100

    Make it run and build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,11 @@ import '@fontsource/montserrat/600.css';
 import React from 'react';
 import Head from 'next/head';
 
-import { PreviewBanner } from 'components/preview-banner';
 import TopNavigation from 'components/top-navigation';
 import { SITE_NAME, SITE_SEO_DESCRIPTION } from 'lib/constants';
 import { AppProps } from 'next/app';
 import { Box, ChakraProvider, CSSReset } from '@chakra-ui/react';
-import theme from './theme';
+import theme from '../lib/theme';
 const LibraryApp = ({ Component, pageProps }: AppProps) => {
   const { ...otherPageProps } = pageProps;
 
@@ -20,7 +19,6 @@ const LibraryApp = ({ Component, pageProps }: AppProps) => {
         <Head>
           <meta name={SITE_NAME} content={SITE_SEO_DESCRIPTION} key="description" />
         </Head>
-        <PreviewBanner />
         <div>
           <TopNavigation />
           <Component {...otherPageProps} />
