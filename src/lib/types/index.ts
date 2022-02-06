70f860085be17f5d10588ab20bbae746ee8a569b
Radek Molenda <radek.molenda@gmail.com>
2022-02-06 09:12:53 +0100

    update types

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,11 +1,7 @@
-import { TypeBlogPost } from './TypeBlogPost';
-
 export type { TypeAddress, TypeAddressFields } from './TypeAddress';
+export type { TypeArticle, TypeArticleFields } from './TypeArticle';
 export type { TypeBlogPost, TypeBlogPostFields } from './TypeBlogPost';
 export type { TypeBranch, TypeBranchFields } from './TypeBranch';
 export type { TypeOpeningTimes, TypeOpeningTimesFields } from './TypeOpeningTimes';
+export type { TypePartner, TypePartnerFields } from './TypePartner';
 export type { TypePerson, TypePersonFields } from './TypePerson';
-
-type TypePage = TypeBlogPost;
-
-export type { TypePage };
