fb11d9730c35517488c2df11863675dee516db94
RadekMolenda <radek.molenda@gmail.com>
2022-04-10 21:16:23 +0200

    Update getHomePage api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { OpeningTimes } from 'components/OpeningTimes';
 import { TopArticles } from 'components/TopArticles';
 import { FetchHomeQuery } from 'generated/types';
 import { getHomePage } from 'lib/api';
-import { ASSETS, HOME_PAGE_ARTICLE_LIMIT } from 'lib/constants';
 import { GetStaticProps } from 'next';
 import React from 'react';
 
@@ -20,8 +19,6 @@ export default function Index({ branches, latestArticles }: FetchHomeQuery) {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { data } = await getHomePage({
     preview: params ? params.preview != undefined : false,
-    heroImageId: ASSETS.heroImageId,
-    articlesLimit: HOME_PAGE_ARTICLE_LIMIT,
   });
 
   return {
